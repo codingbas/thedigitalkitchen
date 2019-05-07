5fc597ed142795ac0201ce38ca5215a0c24cd891
codingbas <basvanhoolwerff1980@gmail.com>
2019-05-07 11:13:47 +0000

    add base.html, basics templates, css, js

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 @app.route('/')
-
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
