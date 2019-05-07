8de827ccba2a963c3a1a5cebdf802263a8b0cb4a
codingbas <basvanhoolwerff1980@gmail.com>
2019-05-07 15:52:28 +0000

    structured the base.html and index.html, add cuisines in mongo

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 @app.route('/')
+
+@app.route('/index')
+def index():
+    """
+    Return index.html which is the first page the user will see
+    """
+    if 'username' in session:
+        return render_template("index.html",
+        recipes=mongo.db.recipes.find(),
+        cuisines=mongo.db.cuisines.find(),
+        categories=mongo.db.categories.find(),
+        difficulties=mongo.db.difficulties.find(),
+        main_ingredients=mongo.db.main_ingredients.find(),
+        allergens=mongo.db.allergens.find(),
+        message='Welcome, '+ str(session['username']) + ', to The Digital Kitchen!')
+    return render_template("index.html", 
+    message='Welcome to The Digital Kitchen!',
+    recipes=mongo.db.recipes.find(),
+    categories=mongo.db.categories.find(),
+    difficulties=mongo.db.difficulties.find(),
+    main_ingredients=mongo.db.main_ingredients.find(),
+    allergens=mongo.db.allergens.find(),
+    cuisines=mongo.db.cuisines.find())
+    
+
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
