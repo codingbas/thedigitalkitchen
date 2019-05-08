f50b28a9579627a16e55bce89db6ccf06f81b954
codingbas <basvanhoolwerff1980@gmail.com>
2019-05-08 07:58:08 +0000

    filled in allrecipes.html and add recipes to mongoDB

--- app.py
+++ app.py
@@ -12,29 +12,14 @@ mongo = PyMongo(app)
 @app.route('/')
 
 @app.route('/index')
-def index():
-    """
-    Return index.html which is the first page the user will see
-    """
-    if 'username' in session:
-        return render_template("index.html",
-        recipes=mongo.db.recipes.find(),
-        cuisines=mongo.db.cuisines.find(),
-        categories=mongo.db.categories.find(),
-        difficulties=mongo.db.difficulties.find(),
-        main_ingredients=mongo.db.main_ingredients.find(),
-        allergens=mongo.db.allergens.find(),
-        message='Welcome, '+ str(session['username']) + ', to The Digital Kitchen!')
+def get_index():
     return render_template("index.html", 
     message='Welcome to The Digital Kitchen!',
     recipes=mongo.db.recipes.find(),
     categories=mongo.db.categories.find(),
-    difficulties=mongo.db.difficulties.find(),
     main_ingredients=mongo.db.main_ingredients.find(),
-    allergens=mongo.db.allergens.find(),
     cuisines=mongo.db.cuisines.find())
     
-
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
